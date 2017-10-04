34e56617edd88eec511a4a9d028bd9c36f5c2531
clousr <clousr@gmail.com>
2017-10-04 15:21:25 -0400

    Built pages via controller generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  # names are connected to the pages_controller file and veiws files
+  get 'pages/home'
+
+  get 'pages/about'
+
+  get 'pages/contact'
+
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
