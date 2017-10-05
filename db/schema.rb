552419f3969308f0ecf4c284119df7055da6520c
clousr <clousr@gmail.com>
2017-10-05 18:23:39 -0400

    created seeds file for sample data and added thumb_image to schema and the particular migration file

--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20171004203017) do
     t.string "subtitle"
     t.text "body"
     t.text "main_image"
+    t.text "thumb_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
