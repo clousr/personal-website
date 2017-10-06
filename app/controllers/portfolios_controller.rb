2bdb51b6436bce39ecb150347f11942759a587d3
clousr <clousr@gmail.com>
2017-10-05 22:27:27 -0400

    integrated index action for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,11 @@
 class PortfoliosController < ApplicationController
+  #this is generally used when you want to list out a number of items
+  def index
+    #class variable = 
+    #model veiw controller
+    #calling the model(Portfolio.all) inside of the controller(this file)
+    #portfolio items now available to the view
+    #now create a new file in views
+    @portfolio_items = Portfolio.all
+  end
 end
