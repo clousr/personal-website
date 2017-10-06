de9cff5e33114677559cdaaae6ef09df17d0ecc0
clousr <clousr@gmail.com>
2017-10-06 19:07:44 -0400

    integrated new-create in portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+
   #this is generally used when you want to list out a number of items
   def index
     #class variable = 
@@ -7,5 +8,21 @@ class PortfoliosController < ApplicationController
     #portfolio items now available to the view
     #now create a new file in views
     @portfolio_items = Portfolio.all
+  end
+
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))#this is another method
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to Portfolio.new, notice: 'Portfolio was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
   end
 end
