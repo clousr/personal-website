0a4ba068288d28f6a71530c1d2bcd6abc5ab4d98
clousr <clousr@gmail.com>
2017-10-21 19:26:35 -0400

    implemented image defaults

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :portfolios, except: [:show]
-  get 'angular-items', to: portfolio_angular
+
+  get 'angular-items', to: 'portfolios#angular'
 
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'#chnages the route to portfolio/:id instead of portfolios/:id. must also change the proper index file
 
